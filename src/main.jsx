4f529a062361628a89746fa9a26f0b3d9cc3e68b
Nhi Huynh <nhi.huynh@hotmail.com>
2025-10-23 18:08:43 +0800

    feat: react router routes scaffolded with todo elements

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRoute, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <BrowserRouter>
+
+      <nav>
+
+      </nav>
+      <Routes>
+
+        <Route path='/' element={<div>TODO!</div>} />
+        <Route path='/about' element={<div>TODO!</div>} />
+        <Route path='/contact' element={<div>TODO!</div>} />
+        <Route path='/articles'>
+          <Route index path='/' element={<div>TODO!</div>} />
+          <Route path='/new' element={<div>TODO!</div>} />
+          <Route path='/:articleId/edit' element={<div>TODO!</div>} />
+          <Route path='/category/:categoryName' element={<div>TODO!</div>} />
+          <Route path='/article/:articleId' element={<div>TODO!</div>} />
+
+        </Route>
+        <Route path='/' element={<div>TODO!</div>} />
+
+      </Routes>
+    </BrowserRouter>
+
   </StrictMode>,
 )
